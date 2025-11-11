a17b0da2bdb2185d55be58de7dbe43843a56d7ec
kp8008 <princekatariyaprince@gmail.com>
2025-11-11 21:23:27 +0530

    44

--- 4_automorphic.c
+++ 4_automorphic.c
@@ -1,2 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
+int main() {
+    int n, square, count = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    square = n * n;
+
+    int temp = n;
+    while (temp != 0) {
+        count++;
+        temp /= 10;
+    }
+
+    int divisor = 1;
+    for (int i = 0; i < count; i++) {
+        divisor = divisor*10;
+    }
+
+    int lastDigits = square % divisor;
+
+    if (lastDigits == n)
+        printf("%d is an Automorphic number.\n", n);
+    else
+        printf("%d is NOT an Automorphic number.\n", n);
+
+    return 0;
+}
