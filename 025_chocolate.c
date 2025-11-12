8ff172ca5b5b186d0980a781a7715876dd19846c
kp8008 <princekatariyaprince@gmail.com>
2025-11-12 08:02:34 +0530

    new

---
+++ 025_chocolate.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main(){
+
+
+    int n=0,m=1,k=0;
+    int tc=0,w=0,rw=0,nc=0;
+ printf("Enter number of chocolate : ");
+    scanf("%d", &n);
+
+     printf("Enter number of k : ");
+    scanf("%d", &k);
+
+    tc=n/m;
+    w=tc;
+
+    while(w>=k){
+        nc=w/k;
+        tc=tc+nc;
+        rw=w%k;
+        w=nc+rw;
+
+    }
+printf(" Total chocolate : %d",tc);
+}
